486b99178897f34f0a1fb27a2a15d80a87f239db
Tõnis Piirits <olen2ge@gmail.com>
2018-07-03 05:30:30 +0300

    implemented positioning via network

--- app/src/main/java/com/weather/tonis/weatherapp/MainActivity.java
+++ app/src/main/java/com/weather/tonis/weatherapp/MainActivity.java
@@ -1,5 +1,12 @@
 package com.weather.tonis.weatherapp;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -33,19 +40,68 @@ public class MainActivity extends AppCompatActivity {
     private List<CityData> cityList = new ArrayList<>();
     private RecyclerView.Adapter adapter;
     private RecyclerView recyclerView;
+    private String[] coordinates;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        loadProducts();
+        getGpsLocation();
         recyclerView = findViewById(R.id.cityList);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        loadProducts();
+
 
     }
+        private void getGpsLocation() {
+        // Acquire a reference to the system Location Manager
+        LocationManager locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+// Define a listener that responds to location updates
+        LocationListener locationListener = new LocationListener() {
+            public void onLocationChanged(Location location) {
+                // Called when a new location is found by the network location provider.
+                makeUseOfNewLocation(location);
+            }
+
+            private void makeUseOfNewLocation(Location location) {
+                coordinates = new String[]{Location.convert(location.getLatitude(), Location.FORMAT_DEGREES), (Location.convert(location.getLongitude(), Location.FORMAT_DEGREES))};
+            }
+
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            public void onProviderEnabled(String provider) {
+            }
+
+            public void onProviderDisabled(String provider) {
+            }
+        };
+
+// Register the listener with the Location Manager to receive location updates
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        String locationProvider = LocationManager.NETWORK_PROVIDER;
+        locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
+        Location lastKnownLocation = locationManager.getLastKnownLocation(locationProvider);
+        convertToGps(lastKnownLocation);
+    }
+
+    private void convertToGps(Location location) {
+        coordinates = new String[]{(Location.convert(location.getLatitude(), Location.FORMAT_DEGREES)), (Location.convert(location.getLongitude(), Location.FORMAT_DEGREES))};
+    }
+
     private void loadProducts() {
-        String dataURL = "http://api.openweathermap.org/data/2.5/box/city?bbox=22,58,28,60,10&APPID=c25e9063552783d77445456d863d5ea7";
+        getGpsLocation();
+        String dataURL = "http://api.openweathermap.org/data/2.5/find?lat=" + coordinates[0] + "&lon=" + coordinates[1] + "&units=metric&cnt=10&APPID=c25e9063552783d77445456d863d5ea7";
         StringRequest stringRequest = new StringRequest(Request.Method.GET, dataURL,
                 (String response) -> {
                     try {
